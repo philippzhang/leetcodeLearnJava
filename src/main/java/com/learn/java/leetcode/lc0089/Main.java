be32a7a54e388507ea2a0df3aaa8fc0060ca0837
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-05-13 14:35:31 +0800

    89. Gray Code 89. 格雷编码 Medium
    Change-Id: I7b70127d5a93a5c8c0e764313bd8601285851cfe

--- src/main/java/com/learn/java/leetcode/lc0089/Main.java
+++ src/main/java/com/learn/java/leetcode/lc0089/Main.java
@@ -11,28 +11,4 @@ public class Main extends CallBack {
 	public static void main(String[] args) {
 		Utilitys.test(Main.class);
 	}
-
-	@Override
-	public boolean outputVerify(Object[] inputObjArr, List<String> trueResultList, Object outputObj, List<String> dataList, List tempList) {
-
-		boolean resultFlag = false;
-		List<Integer> testList = (List<Integer>) outputObj;
-		String testResult = Format.format(testList);
-		for (int i = 0; i < trueResultList.size(); i++) {
-			String trueResult = trueResultList.get(i);
-			if (trueResult.equals("null") && outputObj == null) {
-				printOutVerify(trueResultList, null, true);
-				return true;
-			}
-
-			List<Integer> trueList = Build.buildList(trueResult);
-			resultFlag = Utilitys.compareListsIgnoreOrder(trueList, testList);
-			if (resultFlag) {
-				printOutVerify(trueResultList, testResult, resultFlag);
-				return true;
-			}
-		}
-		printOutVerify(trueResultList, testResult, resultFlag);
-		return resultFlag;
-	}
 }
