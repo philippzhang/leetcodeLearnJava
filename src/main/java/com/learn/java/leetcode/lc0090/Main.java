cce4d6f08a9f0be225669af6f771fd262ddc8a55
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-05-13 14:43:51 +0800

    90. Subsets II 90. 子集 II Medium
    Change-Id: I162dcfc12b123f640a0c639197fa83d6a55245f4

--- src/main/java/com/learn/java/leetcode/lc0090/Main.java
+++ src/main/java/com/learn/java/leetcode/lc0090/Main.java
@@ -9,38 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends CallBack {
-	@Override
-	public boolean outputVerify(Object[] inputObjArr, List<String> trueResultList, Object outputObj, List<String> dataList, List tempList) {
-		boolean resultFlag = false;
-		String testResult = Format.format(outputObj);
-		for (int i = 0; i < trueResultList.size(); i++) {
-			String trueResult = trueResultList.get(i);
-			if (trueResult.equals("null") && outputObj == null) {
-				printOutVerify(trueResultList, null, true);
-				return true;
-			}
-			try {
-				List<List<Integer>> trueResultsList = Build.buildList(trueResult);
-				List<List<Integer>> testResultsList = Build.buildList(testResult);
-
-				resultFlag = Utilitys.compareListsIgnoreOrder(trueResultsList,testResultsList);
-
-				if (resultFlag) {
-					printOutVerify(trueResultList, testResult, resultFlag);
-					return true;
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				printOutVerify(trueResultList, e.getMessage(), false);
-				return false;
-			}
-		}
-
-		printOutVerify(trueResultList, testResult, resultFlag);
-		return resultFlag;
-
-	}
-
 	public static void main(String[] args) {
 		Utilitys.test(Main.class);
 	}
