f8071a3a0e95f3dd50aa6ae94a4907b1e102c61f
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-06-24 16:32:06 +0800

    74. Search a 2D Matrix 74. 搜索二维矩阵 Medium
    Change-Id: I7348374944b39a48f3687631d7c2319fcbec8081

--- src/main/java/com/learn/java/leetcode/lc0074/README.md
+++ src/main/java/com/learn/java/leetcode/lc0074/README.md
@@ -79,6 +79,22 @@ target = 13
 # 测试用例
 ```
 74. Search a 2D Matrix 74. 搜索二维矩阵 Medium
+Solution.searchMatrix
+---
+[[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+3
+
+=true
+---
+[[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+13
+
+=false
+---
+[[1,3]]
+3
+
+=true
 ```
 
 [enTitle]: https://leetcode.com/problems/search-a-2d-matrix/
---
+++ src/main/java/com/learn/java/leetcode/lc0074/Solution.java
@@ -0,0 +1,66 @@
+package com.learn.java.leetcode.lc0074;
+
+public class Solution {
+	public boolean searchMatrix(int[][] matrix, int target) {
+		if(matrix==null||matrix.length==0){
+			return false;
+		}
+		int row = matrix.length;
+		int up = 0, down = row-1;
+		return searchMatrix(matrix,  target, up, down);
+
+	}
+
+	private boolean searchMatrix(int[][] matrix, int target,int up,int down) {
+		if(down<up) {
+			return false;
+		}
+		if(up == down){
+			return searchMatrix(matrix,target,up);
+		}
+		int mid = (up+down)/2;
+		if(matrix[mid][0]==target){
+			return true;
+		}
+		if(matrix[mid][0]>target){
+			return searchMatrix(matrix,  target, up, down-1);
+		}
+		int col = matrix[0].length-1;
+		if(matrix[mid][col]==target){
+			return true;
+		}
+		if(matrix[mid][col]<target){
+			return searchMatrix(matrix,  target, up+1, down);
+		}
+
+		return searchMatrix(matrix,target,mid);
+
+	}
+
+	/**
+	 * 二分搜索
+	 * @param matrix
+	 * @param target
+	 * @param index
+	 * @return
+	 */
+	private boolean searchMatrix(int[][] matrix, int target,int index) {
+		int left = 0;
+		int right = matrix[index].length-1;
+		return searchMatrixBinary(matrix,target,index,left,right);
+	}
+
+	private boolean searchMatrixBinary(int[][] matrix, int target,int index,int left,int right) {
+		if(right<left){
+			return false;
+		}
+		int mid = (left+right)/2;
+		if(matrix[index][mid]==target){
+			return true;
+		}else if(matrix[index][mid]>target){
+			return searchMatrixBinary(matrix,target,index,left,right-1);
+		}else {
+			return searchMatrixBinary(matrix,target,index,left+1,right);
+		}
+	}
+}
