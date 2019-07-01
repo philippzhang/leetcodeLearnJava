5b9e0f9d2c549a0cf306db83a1385ad348ee7db3
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-07-01 16:20:53 +0800

    116. Populating Next Right Pointers in Each Node 116. 填充每个节点的下一个右侧节点指针 Medium 树
    Change-Id: I8ce918cf2772a60576969f66e0f1c916ea2239d4

---
+++ src/main/java/com/learn/java/leetcode/lc0116/Solution.java
@@ -0,0 +1,34 @@
+package com.learn.java.leetcode.lc0116;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+	public Node connect(Node root) {
+		if(root == null||root.left==null&&root.right==null){
+			return root;
+		}
+		Queue<Node> queue = new LinkedList<>();
+		queue.offer(root);
+		while(!queue.isEmpty()){
+			int levelNum = queue.size();
+			Node temp = null;
+			for (int i = 0; i < levelNum; i++) {
+				Node node = queue.poll();
+
+				if(node.left!=null){
+					queue.offer(node.left);
+
+				}
+				if(node.right!=null){
+					queue.offer(node.right);
+				}
+				if(temp!=null){
+					temp.next= node;
+				}
+				temp = node;
+			}
+		}
+		return root;
+	}
+}
