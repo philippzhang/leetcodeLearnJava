4c77ce5184ffa0f81871054e0c9ad472662591b6
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-07-22 14:06:55 +0800

    856. Score of Parentheses 856. 括号的分数 Medium 栈
    Change-Id: I20c960549f736a7ec3c2f60b9b0ec41f513c58b2

--- src/main/java/com/learn/java/leetcode/lc0856/Solution.java
+++ src/main/java/com/learn/java/leetcode/lc0856/Solution.java
@@ -1,7 +1,38 @@
 package com.learn.java.leetcode.lc0856;
 
+import java.util.Stack;
+
 public class Solution {
 	public int scoreOfParentheses(String S) {
-		return 0;
+		Stack<Integer> stack = new Stack<>();
+		char[] chars = S.toCharArray();
+		for(int i =0;i<chars.length;i++){
+			if(chars[i]=='('){
+				stack.push(-1);
+			}else if(chars[i]==')'){
+				int p = stack.peek();
+				if(p==-1){
+					stack.pop();
+					stack.push(1);
+				}else{
+					int t = 0;
+					while(!stack.empty()){
+						int temp = stack.pop();
+						if(temp==-1){
+							break;
+						}else {
+							t +=temp;
+						}
+					}
+					stack.push(t*2);
+				}
+
+			}
+		}
+		int sum =0;
+		while(!stack.empty()){
+			sum+=stack.pop();
+		}
+		return sum;
 	}
 }
