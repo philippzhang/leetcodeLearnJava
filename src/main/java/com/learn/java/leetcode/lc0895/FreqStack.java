ff0a4d0a7cb693012379c4187dab3fdcf43ffa1f
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-07-22 16:39:34 +0800

    895. Maximum Frequency Stack 895. 最大频率栈 Hard 栈
    Change-Id: Ie3989de97cd848ad6e1851bc57a7ab4b348249c0

--- src/main/java/com/learn/java/leetcode/lc0895/FreqStack.java
+++ src/main/java/com/learn/java/leetcode/lc0895/FreqStack.java
@@ -1,15 +1,44 @@
 package com.learn.java.leetcode.lc0895;
 
-public class FreqStack {
-	public FreqStack() {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
+public class FreqStack {
+	Map<Integer,Integer> freqMap;
+	Map<Integer,Stack<Integer>> groupMap;
+	int maxFreq = 0;
+
+	public FreqStack() {
+		freqMap = new HashMap<>();
+		groupMap = new HashMap<>();
+		maxFreq = 0;
 	}
 
 	public void push(int x) {
-
+		int f  = !freqMap.containsKey(x)?1:freqMap.get(x)+1;
+		freqMap.put(x,f);
+		if(groupMap.containsKey(f)){
+			groupMap.get(f).push(x);
+		}else{
+			Stack<Integer> stack = new Stack<>();
+			stack.push(x);
+			groupMap.put(f,stack);
+		}
+		maxFreq = f>maxFreq?f:maxFreq;
 	}
 
 	public int pop() {
-		return 0;
+		if(!groupMap.containsKey(maxFreq)||groupMap.get(maxFreq).empty()){
+			return 0;
+		}else{
+			int x = groupMap.get(maxFreq).pop();
+			freqMap.put(x,freqMap.get(x)-1);
+			if(groupMap.get(maxFreq).empty()){
+				groupMap.remove(maxFreq);
+				maxFreq--;
+			}
+			return x;
+		}
 	}
 }
