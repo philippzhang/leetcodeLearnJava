796b652b2d1cdeab0eaaf8e3ea94f87207a1b9fd
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-07-22 23:25:09 +0800

    921. Minimum Add to Make Parentheses Valid 921. 使括号有效的最少添加 Medium 栈
    Change-Id: Ia04f2d755674b74221a51e514b0f136ee9b27bd4

--- src/main/java/com/learn/java/leetcode/lc0921/Solution.java
+++ src/main/java/com/learn/java/leetcode/lc0921/Solution.java
@@ -1,7 +1,24 @@
 package com.learn.java.leetcode.lc0921;
 
+import java.util.Stack;
+
 public class Solution {
 	public int minAddToMakeValid(String S) {
-		return 0;
+		Stack<Character> stack = new Stack();
+		int result = 0;
+		for(int i=0;i<S.length();i++){
+			char ch = S.charAt(i);
+			if(ch=='('){
+				stack.push(ch);
+			}else{
+				if(!stack.empty()){
+					stack.pop();
+				}else {
+					result++;
+				}
+			}
+		}
+		result +=stack.size();
+		return result;
 	}
 }
