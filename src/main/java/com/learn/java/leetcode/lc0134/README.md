1fd3d15be8f72de0f5230f7585dee6cf6adcf7c6
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-07-26 19:11:53 +0800

    134. Gas Station 134. 加油站 Medium 贪心
    Change-Id: I144907463641b2dbeda336be81676f7b5915444c

--- src/main/java/com/learn/java/leetcode/lc0134/README.md
+++ src/main/java/com/learn/java/leetcode/lc0134/README.md
@@ -117,6 +117,16 @@ Solution.canCompleteCircuit
 [3,4,5,1,2]
 
 =3
+---
+[2,3,4]
+[3,4,3]
+
+=-1
+---
+[5,1,2,3,4]
+[4,4,1,5,1]
+
+=4
 ```
 
 [enTitle]: https://leetcode.com/problems/gas-station/
--- src/main/java/com/learn/java/leetcode/lc0134/Solution.java
+++ src/main/java/com/learn/java/leetcode/lc0134/Solution.java
@@ -1,7 +1,37 @@
 package com.learn.java.leetcode.lc0134;
 
 public class Solution {
+	/**
+	 * 初始化 total 和 current 为 0 ，并且选择 0 号加油站为起点。
+	 *
+	 * 遍历所有的加油站：
+	 *
+	 * 每一步中，都通过加上 gas[i] 和减去 cost[i] 来更新 total 和 current 。
+	 *
+	 * 如果在 i + 1 号加油站， curr < 0 ，将 i + 1 号加油站作为新的起点，同时重置 current = 0 ，让油箱也清空。
+	 *
+	 * 如果 total < 0 ，返回 -1 ，否则返回 starting station。
+	 *
+	 *
+	 * @param gas
+	 * @param cost
+	 * @return
+	 */
 	public int canCompleteCircuit(int[] gas, int[] cost) {
-		return 0;
+		int n = gas.length;
+
+		int total =  0;
+		int current = 0;
+		int start = 0;
+		for(int i =0;i<n;i++){
+			int temp = gas[i]-cost[i];
+			total+=temp;
+			current+=temp;
+			if(current<0){
+				start =i+1;
+				current =0;
+			}
+		}
+		return total>=0?start:-1;
 	}
 }
