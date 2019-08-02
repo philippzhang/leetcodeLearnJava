816f4576627257f52b7943a3393d4c8cd57608d2
zhangyangshuo <yangshuo.zhang@tendcloud.com>
2019-08-02 18:16:46 +0800

    commit
    Change-Id: I7fc43e2c60e1f2940e8e8579686fc01c32d86846

--- src/main/java/com/learn/java/leetcode/base/utils/Format.java
+++ src/main/java/com/learn/java/leetcode/base/utils/Format.java
@@ -89,6 +89,8 @@ public class Format {
 						format((String[]) item);
 					} else if (className.equals("[[Ljava.lang.String;")){
 						format((String[][]) item);
+					} else {
+						format((Object[]) obj, stringBuffer);
 					}
 				} else if(item!=null){
 					throw new RuntimeException("未定义的List泛型，转换失败!");
