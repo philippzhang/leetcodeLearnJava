7b38f76637e7d5ce908ad9a7d1b13e54fd026128
zhangyangshuo <philipp_zhang@163.com>
2021-04-25 17:36:51 +0800

    commit
    Change-Id: Ie6b26f32801d7d52dc37afdc64607c86349e772f

--- src/main/java/com/learn/java/leetcode/offer17/Solution.java
+++ src/main/java/com/learn/java/leetcode/offer17/Solution.java
@@ -2,6 +2,15 @@ package com.learn.java.leetcode.offer17;
 
 public class Solution {
 	public int[] printNumbers(int n) {
-		return null;
+		if(n==0){
+			return new int[0];
+		}
+		int max = (int)Math.pow(10,n)-1;
+
+		int[] ret = new int[max];
+		for(int i=0;i<max;i++){
+			ret[i] = i+1;
+		}
+		return ret;
 	}
 }
